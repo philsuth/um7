685781b3be017ec0190049d8e9eb2fc7e3e4c611
till busch <till@bux.at>
2016-09-21 08:40:58 +0000

    rename to UM7.py, simplify example.py, fix __init__.py

---
+++ um7/um7/__init__.py
@@ -0,0 +1,1 @@
+from .UM7 import *
--- um7/um7/example.py
+++ um7/um7/example.py
@@ -1,24 +1,31 @@
-from um7 import UM7
+import um7
 
 name1 = 's'
-name2 = 'sensor2'
 port1 = '/dev/ttyS0'
-statevars = ['health', 'roll', 'pitch', 'yaw', 'xaccel', 'yaccel', 'zaccel']
-statevars = ['roll', 'pitch', 'yaw', 'xgyroraw', 'ygyroraw', 'zgyroraw', 'xmag', 'ymag', 'zmag']
+statevars = ['health', 'roll', 'pitch', 'yaw', 'xmag', 'ymag', 'zmag', 'xmagraw', 'ymagraw', 'zmagraw', 'xaccelraw', 'yaccelraw', 'zaccelraw', 'xaccel', 'yaccel', 'zaccel', 'xgyro', 'ygyro', 'zgyro']
 
-sensor1 = UM7(name1, port1, statevars)
-sensors = [sensor1]
-for i in sensors:
-    #i.factoryreset()
-    #i.zerogyros()
-    i.resetekf()
-    i.setmagref()
-    i.sethome()
-    i.settimer()
+s1 = um7.UM7(name1, port1, statevars)
 
+print('GET_FW_REVISION=' +     s1.get_fw_revision())
+print('ZERO_GYROS ' + 'ok.' if s1.zero_gyros()      else 'failed.')
+print('RESET_EKF ' + 'ok.'  if s1.reset_ekf()       else 'failed.')
+#s1.reset_to_factory()
+#s1.set_mag_reference()
+#s1.set_home_position()
+
+fs = ''
+hs = ''
+for i in statevars:
+    hs += '{:>9.9s} '
+    if i == 'health':
+        fs += ' {0['+i+']:08b} '
+    else:
+        fs += '{0['+i+']:9.2f} '
+
+c = 0
 while True:
-    #sensor2.grabsample(['xaccel', 'yaccel', 'zaccel', 'xgyro', 'roll', 'pitch', 'yaw'])
-    for i in sensors:
-        #i.catchsample()
-        i.catchallsamples(0.5)
-        print('r= {:8.2f} p= {:8.2f} y= {:8.2f} {:8.2f} {:8.2f} {:8.2f}'.format(i.state['s roll'], i.state['s pitch'], i.state['s yaw'], i.state['s xmag'], i.state['s ymag'], i.state['s zmag']))
+        s1.catchallsamples(0.1)
+        if c % 100 == 0:
+            print(hs.format(*statevars))
+        print(fs.format(s1.state))
+        c += 1
