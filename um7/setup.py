561307054e0adc91dcd7fad0facfded9c0f9038c
Phil Sutherland <phils@rct-global.com>
2021-01-06 11:44:57 +0800

    Add local versioning

--- um7/setup.py
+++ um7/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='um7',
-      version='0.12',
+      version='0.16+rct0',
       description='Classes to interface with CH Robotics / Redshift Labs UM7 IMU',
-      url='https://github.com/buxit/um7',
-      author='Till Busch, Daniel Kurek',
-      author_email='till@bux.at',
+      url='https://github.com/philsuth/um7',
+      author='Till Busch, Daniel Kurek, Phil Sutherland',
+      author_email='phils@rct-global.com',
       license='MIT',
       packages=['um7'],
       install_requires=['pyserial'],
